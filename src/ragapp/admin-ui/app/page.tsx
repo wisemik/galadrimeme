84a125db354cef7e484d1df60c8fbd6326325c54
DmitryPiskarev <piskarev.dmitry25@gmail.com>
2024-08-30 01:42:06 +0300

    about data parser

--- src/ragapp/admin-ui/app/page.tsx
+++ src/ragapp/admin-ui/app/page.tsx
@@ -8,8 +8,15 @@ import { Footer } from "@/sections/footer";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-pink-900 p-4 md:h-52">
         <MemeLogo />
+      </div>
+      <div className="lg:flex lg:justify-between">
+        <div className="lg:w-1/2">Left Content</div>
+        <div className="lg:w-1/2">Right Content</div>
+      </div>
+      <div className="w-full shrink-0">
+        <Footer />
       </div>
     </main>
   );
