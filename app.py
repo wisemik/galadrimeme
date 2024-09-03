13f2540362168120f97a11df9a2875b6b587ea7b
DmitryPiskarev <piskarev.dmitry25@gmail.com>
2024-09-03 12:40:30 +0300

    add news data for 1, 5, and 15 days time window; add functions for news and top headlines downloading.

--- app.py
+++ app.py
@@ -76,31 +76,32 @@ async def send_long_message(message: types.Message, text: str):
         await message.reply(escaped_text, parse_mode="MarkdownV2")
 
 
-def get_all_news(key_words: str = '',
-                 sources: str = '',
-                 time_delta=1,
-                 language='en',
-                 sort_by='relevancy',
-                 save=False) -> None:
+def get_news_data(keywords: str = '',
+                  sources: str = '',
+                  time_delta=1,
+                  language='en',
+                  sort_by='relevancy',
+                  save=False) -> None:
     """
     Note: Free plan enables only 50 requests per 12 hours.
     :param save:
-    :param key_words:
+    :param keywords:
     :param sources:
     :param time_delta:
     :param language:
     :param sort_by:
     :return:
     """
-    assert len(key_words) < 500, 'Maximum size of key_words argument is 500'
-    assert any([s.strip() for s in sources.split(',') if s.strip() in SOURCE_ID]), \
-        f'source must be one of the following: {SOURCE_ID}'
-    assert language in LANGUAGES, f'Language argument must take one of the following values: {LANGUAGES}'
+    suffix_str = '_'.join([x for x in keywords.split(',') if x])
+    assert len(keywords) < 500, 'Maximum size of key_words argument is 500'
+    assert all([s.strip() in SOURCE_ID + [""] for s in sources.split(',')]) or sources is None, \
+        f'Source must be one of the following: {SOURCE_ID + [""]}, got {sources}'
+    assert language in LANGUAGES + [""], f'Language argument must take one of the following values: {LANGUAGES + [""]}'
 
     if sort_by:
         assert sort_by in ['publishedAt', 'relevancy', 'popularity'], f'sort_by value must be in in: ' \
                                                                       f'{["publishedAt", "relevancy", "popularity"]}'
-    assert len([x for x in [key_words, sources] if x != '']) > 0, \
+    assert len([x for x in [keywords, sources] if x != '']) > 0, \
         'At least key_words or sources args must be specified'
 
     all_news = {'articles': []}
@@ -109,7 +110,7 @@ def get_all_news(key_words: str = '',
         try:
             for s in tqdm([x.strip() for x in sources.split(',') if x]):
                 response = news_client.get_everything(
-                    q=key_words,
+                    q=keywords,
                     sources=s,
                     from_param=(datetime.now() - timedelta(days=time_delta)).strftime('%Y-%m-%d'),
                     language=language,
@@ -127,8 +128,7 @@ def get_all_news(key_words: str = '',
     else:
         try:
             response = news_client.get_everything(
-                q=key_words,
-                sources=sources,
+                q=keywords,
                 from_param=(datetime.now() - timedelta(days=time_delta)).strftime('%Y-%m-%d'),
                 language=language,
                 sort_by=sort_by,
@@ -143,16 +143,101 @@ def get_all_news(key_words: str = '',
             pass
 
     if all_news['articles'] and save:
-        with open('news_data.json', 'w') as f:
+        with open(f'{os.getcwd()}/data/news_recent_{language}_{sort_by}_last_{time_delta}d_{suffix_str}_data.json',
+                  'w') as f:
             json.dump(all_news, f)
 
     else:
-        logger.info(e)
+        logger.info("Nothing to store")
+
+
+def get_top_headlines(keywords: str = '',
+                      sources: str = '',
+                      category: str = '',
+                      language: str = 'en',
+                      country: str = '') -> list:
+    assert len(keywords) < 500, 'Maximum size of key_words argument is 500'
+    assert all([s.strip() in SOURCE_ID + [""] for s in sources.split(',')]) or sources is None, \
+        f'Source must be one of the following: {SOURCE_ID + [""]}, got {sources}'
+    assert language in LANGUAGES + [""], f'Language argument must take one of the following values: {LANGUAGES + [""]}'
+    assert country in COUNTRIES + [""], f'Country argument must take one of the following values: {COUNTRIES + [""]}'
+    assert category in CATEGORIES + [
+        ""], f'Category argument must take one of the following values: {CATEGORIES + [""]}'
+    assert len([x for x in [keywords, sources, category] if x != '']) > 0, \
+        'At least keywords, sources, or category args must be specified'
+
+    if not sources:
+        res = news_client.get_top_headlines(
+            q=keywords,
+            category=category,
+            language=language,
+            # country=country,
+            page_size=100)
+    else:
+        res = news_client.get_top_headlines(
+            q=keywords,
+            sources=sources,
+            language=language,
+            page_size=100)
+
+    if res['status'] == 'ok':
+        return res['articles']
+
+    else:
+        return []
+
+
+def get_all_cat_top_headlines(keywords: str = '', categories=None, dump: bool = True):
+    suffix = 'all' if not keywords else '_'.join([w.strip() for w in keywords.split(',') if w])
+
+    if categories is None:
+        data = get_top_headlines(keywords=keywords)
+
+    else:
+        data = []
+        for category in tqdm(categories):
+            res = get_top_headlines(keywords=keywords, category=category)
+            data.extend(res)
+
+    if dump:
+        logger.info(f"Saving result to {f'/data/news_recent_top_en_{suffix}.json ...'}")
+        with open(f'{os.getcwd()}/data/headlines_recent_top_en_{suffix}.json', 'w') as f:
+            json.dump(data, f)
+        logger.info('Done.')
+
+    return data
+
+
+def get_all_headlines(categories: list) -> None:
+    keywords = [
+        # None,
+        # 'politics', 'environment', 'crisis', 'selections', 'ev', 'crypto',
+        'bitcoin', 'ai', 'gpt', 'ethereum', 'finance', 'llm',
+    ]
+    for kw in keywords:
+        logger.info(f'Getting and saving data for {kw} keyword...')
+        if kw is None:
+            get_all_cat_top_headlines(categories=categories)
+        else:
+            get_all_cat_top_headlines(keywords=kw, categories=categories)
+
+
+def get_all_news() -> None:
+    keywords = [
+        'global',
+        'politics', 'environment', 'crisis', 'selections', 'ev', 'crypto',
+        'bitcoin', 'ai', 'gpt', 'ethereum', 'finance', 'llm',
+    ]
+    for kw in keywords:
+        logger.info(f'Getting and saving data for {kw} keyword...')
+        get_news_data(keywords=kw, time_delta=15, save=True)
 
 
 async def main():
     print("Starting bot polling")
-    get_all_news(time_delta=5, sources=','.join(SOURCE_ID), save=True)
+
+    # get_all_headlines(categories=CATEGORIES)
+    get_all_news()
     # await dp.start_polling(bot)
 
 
