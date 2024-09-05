c97914fa5daddbd743aa3b5796b5092040642589
DmitryPiskarev <piskarev.dmitry25@gmail.com>
2024-09-05 12:12:38 +0300

    set up schema, add attestation

--- sign_protocol.ts
+++ sign_protocol.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import axios from "axios";
 dotenv.config({ path: __dirname+'/.env' });
 import { secp256k1 } from 'ethereum-cryptography/secp256k1';
 
@@ -7,31 +8,51 @@ const { privateKeyToAccount } = require("viem/accounts");
 
 const privateKey = process.env.ETH_ACCOUNT_KEY;
 const accountAddress = process.env.ETH_ACCOUNT_ADDRESS;
-const contractDetails = "A coin tending to prove that recycling is a boon."
+const tokenDetails = "A coin tending to prove that recycling is a boon."
 
 const client = new SignProtocolClient(SpMode.OnChain, {
   chain: EvmChains.baseSepolia,
   account: privateKeyToAccount(privateKey),
 });
 
-async function test() {
+async function createSchema() {
     const res = await client.createSchema({
-      name: "SDK Test",
+      name: "Token Purpose Verification",
       data: [
-        { name: contractDetails, type: "string" },
-        { name: "accountAddress", type: "address" },
+        { name: "tokenDetails", type: "string" },
+        { name: "signer", type: "address" },
       ],
     });
 
-    console.log(res)
-
     return res
 }
-//const res = await client.createSchema({
-//  name: "SDK Test",
-//  data: [
-//    { name: contractDetails, type: "string" },
-//    { name: "accountAddress", type: "address" },
-//  ],
-//});
-test();
+
+async function createNotaryAttestation(schemaId: string, tokenDetails: string, signer: string) {
+  const res = await client.createAttestation({
+    schemaId: schemaId,
+    data: {
+      tokenDetails,
+      signer
+    },
+    indexingValue: signer.toLowerCase()
+  });
+
+  return res
+}
+
+
+// create Schema
+(async() => {
+  console.log('creating schema...');
+  const schema = await createSchema();
+//  const schema = '0x1b8';
+  console.log('schema: ', schema);
+  console.log('Done.');
+  console.log('creating attestation...');
+  const attestation = await createNotaryAttestation(schema['schemaId'], tokenDetails, accountAddress as string);
+  console.log('attestation: ', attestation);
+  console.log('Done.');
+})();
+
+
+
